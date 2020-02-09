b6c6ef834bca0aeb8a662a25df8b12d5bed1b12c
zmeriksen <zmeriksen@gmail.com>
2020-02-08 18:08:04 -0600

    Added public to Account fields

--- Sources/GW2/Models/Account.swift
+++ Sources/GW2/Models/Account.swift
@@ -8,18 +8,18 @@
 import Foundation
 
 public struct Account: Codable {
-    let id: String
-    let age: Int
-    let name: String
-    let world: Int
-    let guilds: [String]
-    let guild_leader: [String]
-    let created: String
-    let access: [String]
-    let commander: Bool
-    let fractal_level: Int
-    let daily_ap: Int
-    let monthly_ap: Int
-    let wvw_rank: Int
-    let last_modified: String?
+    public let id: String
+    public let age: Int
+    public let name: String
+    public let world: Int
+    public let guilds: [String]
+    public let guild_leader: [String]
+    public let created: String
+    public let access: [String]
+    public let commander: Bool
+    public let fractal_level: Int
+    public let daily_ap: Int
+    public let monthly_ap: Int
+    public let wvw_rank: Int
+    public let last_modified: String?
 }
