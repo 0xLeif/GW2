7bdf5cada0d9aca3e75e713ef120fbbae0d149bb
zmeriksen <zmeriksen@gmail.com>
2020-02-09 10:27:47 -0600

    Added extensions for unit tests with combine

---
+++ Tests/GW2Tests/Extensions/XCTest+.swift
@@ -0,0 +1,26 @@
+//
+//  XCTest+.swift
+//  
+//
+//  Created by Zach Eriksen on 2/9/20.
+//
+
+import Foundation
+import XCTest
+
+extension XCTest {
+    func expectToEventually(_ test: @autoclosure () -> Bool, timeout: TimeInterval = 1.0, message: String = "") {
+        let runLoop = RunLoop.current
+        let timeoutDate = Date(timeIntervalSinceNow: timeout)
+        repeat {
+            // 1
+            if test() {
+                return
+            }
+            // 2
+            runLoop.run(until: Date(timeIntervalSinceNow: 0.01))
+        } while Date().compare(timeoutDate) == .orderedAscending // 3
+        // 4
+        XCTFail(message)
+    }
+}
