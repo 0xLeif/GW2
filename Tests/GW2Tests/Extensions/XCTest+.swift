59f88b119dd9ffc2754f13dc17834f4c054967da
zmeriksen <zmeriksen@gmail.com>
2020-02-09 10:28:19 -0600

    Gave ACK to vadimbulavin

--- Tests/GW2Tests/Extensions/XCTest+.swift
+++ Tests/GW2Tests/Extensions/XCTest+.swift
@@ -8,19 +8,20 @@
 import Foundation
 import XCTest
 
+/// ACK: [vadimbulavin](https://www.vadimbulavin.com/swift-asynchronous-unit-testing-with-busy-assertion-pattern/)
 extension XCTest {
-    func expectToEventually(_ test: @autoclosure () -> Bool, timeout: TimeInterval = 1.0, message: String = "") {
+    func expectToEventually(_ test: @autoclosure () -> Bool, timeout: TimeInterval = 5.0, message: String = "") {
         let runLoop = RunLoop.current
         let timeoutDate = Date(timeIntervalSinceNow: timeout)
         repeat {
-            // 1
+            
             if test() {
                 return
             }
-            // 2
+            
             runLoop.run(until: Date(timeIntervalSinceNow: 0.01))
-        } while Date().compare(timeoutDate) == .orderedAscending // 3
-        // 4
+        } while Date().compare(timeoutDate) == .orderedAscending
+        
         XCTFail(message)
     }
 }
